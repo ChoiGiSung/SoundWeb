bc1b2600b65cd6664150c39691aaebc1944f62b1
ChoiGiSung <chlrltjd159@naver.com>
2020-09-17 21:51:28 +0900

    리액트 수정

--- sound-web/src/main/soundweb-frontend/config/paths.js
+++ sound-web/src/main/soundweb-frontend/config/paths.js
@@ -69,7 +69,9 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
+
   appBuild: resolveApp('build/static'),
+
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
