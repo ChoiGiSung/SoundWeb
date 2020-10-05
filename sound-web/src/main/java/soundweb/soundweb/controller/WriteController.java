47cbdbcce89b2cc11072760c013b6d587b6a0e4b
ChoiGiSung <chlrltjd159@naver.com>
2020-10-05 23:13:57 +0900

    쿠키값을 이용한 방문수 계산

--- sound-web/src/main/java/soundweb/soundweb/controller/WriteController.java
+++ sound-web/src/main/java/soundweb/soundweb/controller/WriteController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import soundweb.soundweb.Domain.Entity.BoardEntity;
@@ -14,6 +15,9 @@ import soundweb.soundweb.Domain.Repositoty.UserRepository;
 import soundweb.soundweb.Domain.Service.BoardService;
 import soundweb.soundweb.Domain.Service.UserService;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -37,7 +41,7 @@ public class WriteController {
 
     //글쓰기 완료시
     @PostMapping("/write")
-    public String writePost(WriteDtoForm writeDtoForm, Model model){
+    public String writePost(WriteDtoForm writeDtoForm){
         logger.info(writeDtoForm.getTitle()+"내용"+writeDtoForm.getContent());
         
         //세션을 이용한 로그인 아이디 넘겨주는 작업 필요
@@ -45,11 +49,47 @@ public class WriteController {
         return "/Home/main";
     }
 
+    //게시글 목록
     @GetMapping("/allpost")
-    public String AllPost(Model model){
+    public String AllPost(Model model,
+                         // HttpServletRequest httpServletRequest,
+                          @CookieValue(value = "count",defaultValue ="0",required = true )String value,
+                          HttpServletResponse httpServletResponse)
+    {
 
         List<BoardEntity> findPostALl = boardService.ReadPostAll();
         model.addAttribute("AllPost",findPostALl);
+
+        //쿠키함수
+//        String value=null;
+//        boolean find =false;
+//        Cookie[]cookies=httpServletRequest.getCookies();
+//        if(cookies !=null){
+//            for (Cookie cookie:cookies){
+//                if ("count".equals(cookie.getName())){
+//                    find=true;
+//                    value=cookie.getValue();
+//                    break;
+//                }
+//            }
+//        }
+//        if (!find) {
+//            value = "1";
+//        }else {
+            try {
+                int i=Integer.parseInt(value);
+                value=Integer.toString(++i);
+            }catch (Exception e){
+                value="1";
+            }
+       // }
+        Cookie cookie=new Cookie("count",value);
+        cookie.setMaxAge(60*60*24*365);
+        cookie.setPath("/");
+        httpServletResponse.addCookie(cookie);
+
+        model.addAttribute("cookieCount",value);
+
 
         return "/AllPost";
     }
